6eb7d70a939684d78c68901d2be889a7cad855c1
laliproj <lalitechzone@gmail.com>
2023-02-11 14:17:00 +0300

    Add welcome message output

--- src/Main.java
+++ src/Main.java
@@ -4,3 +4,5 @@ import java.*
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
