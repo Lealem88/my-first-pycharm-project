4d6be3a0596b9f1529c77f3ee599ac35c2434815
laliproj <lalitechzone@gmail.com>
2023-02-21 11:52:00 +0300

    Add infinite while loop for guessing

--- src/pp.py
+++ src/pp.py
@@ -4,3 +4,5 @@ def main(): #function
     while True:
         guess = int(input("Guess a number between 1 and 10: "))
         if guess > number:
+            print("Too high")
+            continue
